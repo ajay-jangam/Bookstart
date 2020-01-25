8b9422da512b3cb83d0cebaa1e9f967c59211a7a
Ajay Jangam <Ajay Jangam>
2020-01-26 02:24:36 +0530

    Modify search button with hover effect

--- slick-script.js
+++ slick-script.js
@@ -45,7 +45,7 @@ $(document).ready(function(){
   });
 
   $("#topBtn").click(function(){
-    $('html ,body').animate({scrollTop : 0},800);
+    $('html ,body').animate({scrollTop : 0},1500);
   });
 });
 
