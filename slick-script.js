c697d79be1a7ac3f1dc54dfd1fb36a98939eaf33
Ajay Jangam <Ajay Jangam>
2020-01-25 23:29:07 +0530

    Bookstart desktop version complete

---
+++ slick-script.js
@@ -0,0 +1,53 @@
+// slick-slider-1
+
+$(document).ready(function(){
+    $('.single-item').slick({
+        slidesToShow: 1,
+        infinite: true,
+        speed: 1000,
+        dots: true,
+        nextArrow: ('.next'),
+        prevArrow: ('.prev'),
+    });
+  });
+
+// slick-slider-1
+
+
+// slick-slider-2
+
+$(document).ready(function(){
+  $('.single-item-1').slick({
+      slidesToShow: 1,
+      infinite: true,
+      speed: 1000,
+      dots: false,
+      nextArrow: ('.event-next'),
+      prevArrow: ('.event-prev'),
+  });
+});
+
+// slick-slider-2
+
+
+// srcoll to top
+
+//Get the button:
+
+$(document).ready(function(){
+
+  $(window).scroll(function(){
+    if($(this).scrollTop() > 40){
+      $('#topBtn').fadeIn();
+    } else{
+      $('#topBtn').fadeOut();
+    }
+  });
+
+  $("#topBtn").click(function(){
+    $('html ,body').animate({scrollTop : 0},800);
+  });
+});
+
+// scroll to top
+
